a6d4305e063f22bb267a933d71a6826c9c4f09ef
charan <saicharanbhonagiri02@gmail.com>
2026-09-24 14:48:47 +0530

    update

--- server/routes/courseRoutes.js
+++ server/routes/courseRoutes.js
@@ -6,7 +6,7 @@ const courseRoute = express.Router()
 
 courseRoute.get("/",getCourses)
 
-courseRoute.post("/", protect, authorize('instructor', 'student'), createCourse)
+courseRoute.post("/", protect, authorize('instructor', 'admin'), createCourse)
 
 
 courseRoute.get("/:id", getCourseById)
